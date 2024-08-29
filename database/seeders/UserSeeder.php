0b216fdb3ef5da54a5b01d13b0fb884909fee482
tbunitrade <tbunitrade@gmail.com>
2024-08-29 23:36:09 +0300

    save progress

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 45; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password'), // Устанавливаем стандартный пароль
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Маршрут для загрузки изображения
+Route::get('/upload-form', function () {
+    return view('upload'); // Показываем форму загрузки
+});
+
+Route::post('/upload-image', [ImageController::class, 'upload']); // Загружаем и обрабатываем изображение
